38a4a24d61c7b51665641eee364cc6e79a8ec28c
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2024-09-12 14:40:56 +0200

    Update view and rename templates

--- django_playground/stocks/views.py
+++ django_playground/stocks/views.py
@@ -9,17 +9,17 @@ from django_playground.stocks.models import StockMovement
 class StockListView(ListView):
     model = Product
     queryset = Product.objects.prefetch_related("category", "supplier").all()
-    template_name = "stocks/all_stocks.html"
+    template_name = "stocks/products/list.html"
     paginate_by = 20
 
 
 class StockDetailView(DetailView):
     model = Product
-    template_name = "stocks/product_detail.html"
+    template_name = "stocks/products/detail.html"
 
 
 class StockMovementListView(ListView):
     model = StockMovement
     queryset = StockMovement.objects.prefetch_related("product").all()
-    template_name = "stocks/stock_movements.html"
+    template_name = "stocks/movements/list.html"
     paginate_by = 20
