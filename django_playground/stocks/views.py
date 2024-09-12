a56dd28a7548d0d44ebe27e768200e3157850063
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2024-09-12 14:41:54 +0200

    [WIP] Add form for stock movement

--- django_playground/stocks/views.py
+++ django_playground/stocks/views.py
@@ -1,7 +1,10 @@
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from django_playground.stocks.forms import StockMovementForm
 from django_playground.stocks.models import Product
 from django_playground.stocks.models import StockMovement
 
@@ -23,3 +26,12 @@ class StockMovementListView(ListView):
     queryset = StockMovement.objects.prefetch_related("product").all()
     template_name = "stocks/movements/list.html"
     paginate_by = 20
+
+
+class StockMovementCreateView(CreateView):
+    model = StockMovement
+    form_class = StockMovementForm
+    template_name = "stocks/movements/form.html"
+
+    def get_success_url(self):
+        return reverse("stocks:product-detail", kwargs={"pk": self.object.product.pk})
