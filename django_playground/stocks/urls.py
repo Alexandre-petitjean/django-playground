d077bda65c19b17db39c82c897af2f50bba2af0b
Alexandre Petitjean <alexandre.petitjean@itsf.io>
2024-09-18 16:27:58 +0200

    New template and form

--- django_playground/stocks/urls.py
+++ django_playground/stocks/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
-from django_playground.stocks.views import StockDetailView
+from django_playground.stocks.views import StockDetailView, StockDashboard, ProductBurnView
 from django_playground.stocks.views import StockListView
 from django_playground.stocks.views import StockMovementCreateView
 from django_playground.stocks.views import StockMovementListView
 
 app_name = "stocks"
 urlpatterns = [
-    path("", view=StockListView.as_view(), name="stocks"),
+    path("", view=StockDashboard.as_view(), name="stocks"),
+    path("products/", view=StockListView.as_view(), name="products"),
     path("products/<slug:pk>/", view=StockDetailView.as_view(), name="product-detail"),
+    path("products/<slug:pk>/burn/", view=ProductBurnView.as_view(), name="product-stock-burn"),
+    path("products/<slug:pk>/new-movement/", view=StockMovementCreateView.as_view(), name="product-new-movement"),
     path("movements/", view=StockMovementListView.as_view(), name="stock-movements"),
-    path("products/<slug:pk>/new-movement/", view=StockMovementCreateView.as_view(), name="stock-movements-new"),
 ]
