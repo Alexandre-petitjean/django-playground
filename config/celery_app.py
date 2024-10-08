6396e334a9e8274cb6b8804289467b5b8c36ae93
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2024-10-08 09:04:11 +0200

    WIP dlx

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from kombu import Exchange
+from kombu import Queue
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -15,3 +17,28 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+# Configuration des exchanges et des queues
+app.conf.task_queues = (
+    # Queue normale pour Celery
+    Queue(
+        "celery",
+        Exchange("celery", type="direct"),
+        routing_key="celery",
+        queue_arguments={
+            "x-dead-letter-exchange": "dead_letter_exchange",  # DLX exchange
+            "x-dead-letter-routing-key": "dead_letter_routing_key",  # DLX routing key
+        },
+    ),
+    # Dead Letter Queue (pour les messages échoués)
+    Queue(
+        "dead_letter_queue",
+        Exchange("dead_letter_exchange", type="direct"),
+        routing_key="dead_letter_routing_key",
+    ),
+)
+
+# Optionnel : configuration des routes si vous voulez spécifier quelle tâche va dans quelle queue
+# app.conf.task_routes = {
+#     'myapp.tasks.my_task': {'queue': 'celery'},  # Exemple de route pour une tâche
+# }
